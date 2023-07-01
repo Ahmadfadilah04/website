6e3c676e7bfd67e2fcdc554f2771853c279c8bf5
Ahmadfadilah04 <138303911+Ahmadfadilah04@users.noreply.github.com>
2023-07-01 20:12:40 +0700

    Add files via upload

---
+++ bc _web/script.js
@@ -0,0 +1,46 @@
+const navEl = document.querySelector('.nav');
+const hamburgerEl = document.querySelector('.hamburger');
+const navItemEls = document.querySelectorAll('.nav__item');
+
+hamburgerEl.addEventListener('click', () => {
+  navEl.classList.toggle('nav--open');
+  hamburgerEl.classList.toggle('hamburger--open');
+});
+
+navItemEls.forEach(navItemEl => {
+  navItemEl.addEventListener('click', () => {
+    navEl.classList.remove('nav--open');
+    hamburgerEl.classList.remove('hamburger--open');
+  });
+});
+
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
